74268573caaba6c3444de0051476c6055078267c
Rawan Elkhouly <52850403+rawaann@users.noreply.github.com>
2023-10-17 19:38:24 +0300

    check

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+import express from 'express';
+import bodyParser from 'body-parser';
+// const express = require('express')
+// const bodyParser = require('body-parser')
 const app = express();
 
 app.use(bodyParser.urlencoded({extended : true}))
