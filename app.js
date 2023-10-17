f843c0ab6ee39e0aea1e998e2ea6ec12e96b2fd8
Rawan Elkhouly <52850403+rawaann@users.noreply.github.com>
2023-10-17 17:14:12 +0300

    forth commit

--- app.js
+++ app.js
@@ -13,6 +13,24 @@ app.get('/', function(req,res){
     res.sendFile(__dirname + "/index.html")
 })
 
+app.get('/merchant-session/new', function(req, res) {
+    var uri = req.query.validationURL || 'https://apple-pay-gateway-cert.apple.com/paymentservices/startSession';
+  
+    var options = {
+      uri: uri,
+      json: {
+        merchantIdentifier: 'merchant.boubyantakaful.pay',
+        domainName: 'applepay-woad.vercel.app',
+        displayName: 'Boubyan Takaful'
+      },
+  
+      agentOptions: {
+        cert: cert,
+        key: cert
+      }
+    };
+});
+
 app.post('/', function(req, res){
     
 })
