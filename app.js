19bf901362a8f4390bde134336e81d26fbb4a752
Rawan Elkhouly <52850403+rawaann@users.noreply.github.com>
2023-10-18 08:28:12 +0300

    add2

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
 // import expres 'express';
 // import bodyParser from 'body-parser';
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
-app.use(bodyParser.urlencoded({extended : true}))
+// app.use(bodyParser.urlencoded({extended : true}))
+app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.listen(process.env.PORT, function(){
-    console.log("server is running")
-})
 
 app.get('/', function(req,res){
-    res.sendFile(__dirname + "/index.html")
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 app.post('/applepaysession', function(req, res) {
-    var uri = req.query.validationURL || 'https://apple-pay-gateway-cert.apple.com/paymentservices/startSession';
-    console.log(uri);
-    var options = {
-      uri: uri,
-      json: {
-        merchantIdentifier: 'applepay-woad.vercel.app',
-        domainName: 'merchant.boubyantakaful.pay',
-        displayName: 'Testing Apple Pay'
-      },
-    };
-
-    req.post(options, function(error, response, body) {
-        if (body) {
-          // Apple returns a payload with `displayName`, but passing this
-          // to `completeMerchantValidation` causes it to error.
-          console.log(body);
-          delete body.displayName;
-        }
-        console.log(body);
-        res.send(body);
-      });
+  var uri = req.query.validationURL || 'https://apple-pay-gateway-cert.apple.com/paymentservices/startSession';
+  console.log(uri);
+  var options = {
+    uri: uri,
+    json: {
+      merchantIdentifier: 'applepay-woad.vercel.app',
+      domainName: 'merchant.boubyantakaful.pay',
+      displayName: 'Testing Apple Pay'
+    },
+  };
+  
+  req.post(options, function(error, response, body) {
+    if (body) {
+      // Apple returns a payload with `displayName`, but passing this
+      // to `completeMerchantValidation` causes it to error.
+      console.log(body);
+      delete body.displayName;
+    }
+    console.log(body);
+    res.send(body);
+  });
 });
 // app.post('/', function(req, res){
-    
-// })
-
+  
+  // })
+  
+app.listen(process.env.PORT, function(){
+      console.log("server is running")
+  })
 module.exports.app = app;
