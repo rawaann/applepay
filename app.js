ca31016ea5e839f455faf4c999f5707df3927767
Rawan Elkhouly <52850403+rawaann@users.noreply.github.com>
2023-10-18 09:45:48 +0300

    test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  // res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 app.post('/applepaysession', function(req, res) {
